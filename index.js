ebe81b68c4c605023cd6bc3717c3d28e423c0d6a
Robins <robinmgk077@gmail.com>
2023-12-14 00:23:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(argument = "roller-skate") {
+  return `This Saturday, I want to ${argument}!`;
+}
+
+function mondayWork(argument2 = "go to the office") {
+  return `This Monday, I will ${argument2}.`;
+}
+
+function wrapAdjective(flair = "*") {
+  return function (argument3 = "special") {
+    return `You are ${flair}${argument3}${flair}!`;
+  };
+}
